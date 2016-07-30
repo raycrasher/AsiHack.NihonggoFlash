6675939b7d6e467a82fc43c72faa2ee3a785b7cc
David Garcia <cinquedia@gmail.com>
2016-07-30 11:22:17 +0800

    Initial Commit

---
+++ Alliance.NihonggoFlash.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alliance.NihonggoFlash.Core
+{
+    public class Class1
+    {
+    }
+}
